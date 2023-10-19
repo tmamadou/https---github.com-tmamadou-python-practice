1274349eb3b2073c9e40843f9b3b3448cd133a81
tmamadou <tmamadou2003@gmail.com>
2023-10-19 14:24:53 -0400

    First Commit

---
+++ LAB3.py
@@ -0,0 +1,4 @@
+print("I will convert your celsius to fahrenheit, inch to centimeters, and pounds to kilos.")
+celsius = input("Please enter a number for Celsius degrees:")
+inch = input("Please enter a number for inch:")
+pounds = input("Please enter a number for pounds :")
